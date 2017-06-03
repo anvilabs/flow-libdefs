59379b83569f9074d8dc7cf2504a5bd70df8c8d5
Ayan Yenbekbay <ayan.yenb@gmail.com>
2017-06-03 10:25:55 +0600

    chore(deps): upgrade to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['anvilabs', 'anvilabs/flowtype'],
   rules: {
+    // https://github.com/gajus/eslint-plugin-flowtype
+    'flowtype/no-weak-types': 'off',
     // https://github.com/benmosher/eslint-plugin-import
-    'import/no-extraneous-dependencies': 0,
+    'import/no-extraneous-dependencies': 'off',
   },
 };
